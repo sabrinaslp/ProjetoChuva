71f01a7336ad1d5bfcf4cd0872da27d6888be4ab
Sabrina Satriany Lima Pereira <sabrinaslimap@gmail.com>
2022-05-15 21:35:46 -0300

    Adicionando a interação do site com JS

--- js/script.js
+++ js/script.js
@@ -3,16 +3,19 @@
 var showContent = document.getElementById("showContent"); // ver mais
 var textResume = document.getElementById("textResume"); // texto expandido
 var resumeContent = document.getElementById("resumeContent"); // todo o conteúdo de texto
+var postDiscussion = document.getElementById("postDiscussion"); // seção de discussões
 
 function showHiddenText() {
     if (showContent.style.display === "none") {
      showContent.style.display = "inline";
      textResume.style.display = "none";
      resumeContent.style.height = '144px';
+     postDiscussion.style.marginTop = '0';
     } else {
       showContent.style.display = "none";
       textResume.style.display = "block";
       resumeContent.style.height = '390px';
+      postDiscussion.style.marginTop = "250px";
     }
   }
 
@@ -37,13 +40,66 @@ function hiddenMenu() {
   logoSide.style.display = "none";
 }
 
+// Criar novo tópico
+
+var createTopic = document.getElementById("createTopic");
+var topics = document.getElementById("topics");
+var createTopicButton = document.getElementById("createTopicButton");
+var homeTopic = document.getElementById("homeTopic");
+
+function newTopic() {
+  topics.style.position = "absolute"; 
+  topics.style.width = "100%"; 
+  topics.style.marginTop = "73px";
+  createTopic.style.display = "block";
+  homeTopic.style.display = "none";
+  expandedTopic.style.display = "none";
+  postDiscussion.style.height = "700px";
+  sentTopic.style.display = "none";
+  footerPage.style.marginTop = "100px";
+}
+
 // Enviar novo tópico
 
-// var createTopic = document.getElementById("createTopic");
-// var topics = document.getElementById("topics");
+var sentTopic = document.getElementById("sentTopic");
+var submittedTopic = document.getElementById("submittedTopic");
+var sendTopicButton = document.getElementById("sendTopicButton");
 
-// function newTopic() {
-  
-// }
+function sendTopic() {
+  createTopic.style.display = "none";
+  sentTopic.style.display = "block";
+  submittedTopic.style.display = "block";
+  topics.style.marginTop = "150px";
+  topics.style.left = "1px";
+  expandedTopic.style.display = "none";
+  footerPage.style.marginTop = "160px";
+  postDiscussion.style.height = "753px";
+}
+
+// Expandir tópico
+
+var collapsedTopics = document.getElementById("collapsedTopics");
+var expandedTopic = document.getElementById("expandedTopic");
+var postDiscussion = document.getElementById("postDiscussion");
+
+function expandTopic() {
+  if (sentTopic.style.display === "none") {
+    expandedTopic.style.display = "block";
+    postDiscussion.style.height = "1170px";
+    expandedTopic.style.marginTop = "78px";
+    expandedTopic.style.height = "600px";
+    expandedTopic.style.position = "Absolute";
+    expandedTopic.style.left = "5px";
+    footerPage.style.marginTop = "650px";
+  } else {
+    expandedTopic.style.display = "block";
+    expandedTopic.style.marginTop = "0px";
+    expandedTopic.style.height = "600px";
+    postDiscussion.style.height = "1170px";
+    footerPage.style.marginTop = "550px";
+  }
+}
+
+var footerPage = document.getElementById("footerPage");
 
   
